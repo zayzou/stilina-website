1a8e522a82f4b8b675062fc4d2719a7fcefe4559
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-12-20 14:46:45 -0500

    setup Navbar.jsx

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "../components";
+import { Header, Navbar } from "../components";
 function HomeLayout() {
   return (
     <>
       <Header />
+      <Navbar />
       <section className="align-element  py-20">
         <Outlet />
       </section>
