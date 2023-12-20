077613ccba7c4fd9f8884458ccd9acaccbed211f
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-12-20 14:49:32 -0500

    fix Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,24 +9,34 @@ function Navbar() {
     <nav className="bg-base-200">
       <div className="navbar align-element">
         <div className="navbar-start">
+          {/* Title */}
           <NavLink
             to="/"
             className="hidden lg:flex btn btn-primary text-3xl items-center "
           >
             S
           </NavLink>
+          {/* DROPDOWN */}
           <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden"></label>
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <FaBarsStaggered className="h-6 w-6" />
+            </label>
             <ul
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
-            ></ul>
+            >
+              {" "}
+              nav links
+            </ul>
           </div>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal"></ul>
+          <ul className="menu menu-horizontal">nav links</ul>
         </div>
+
         <div className="navbar-end">
+          {/* THEME ICONS */}
+          {/* CART LINK*/}
           <NavLink to="/cart" className="btn btn-ghost btn-circle btn-md ml-4">
             <div className="indicator">
               <BsCart3 className="h-6 w-6" />
