8d8ea2f835f74c527cac94a4b52d6dae234951d1
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-12-22 14:15:34 -0500

    under construction page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,7 @@
 import { BsArrowRight } from "react-icons/bs";
 import Lottie from "lottie-react";
 import animationData from "../assets/animation02.json";
+import pen from "../assets/images/pen.png";
 const About = () => {
   return (
     <div className="grid grid-rows-1 gap-6 ">
@@ -30,25 +31,30 @@ const About = () => {
             pour sa marque, a occupé une place de premier plan sur le marché.
           </p>
           <a href="#histoire" className="mt-4 btn btn-neutral ">
-            Notre histoire
+            Nos valeurs
             <BsArrowRight />
           </a>
         </div>
       </div>
 
-      <div
-        className="card lg:card-side bg-base-300 shadow-xl min-h-[75vh] w-100"
-        id="valeurs"
-      >
-        <figure className="h-[100]">
-          <img
-            src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
-            alt="Album"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">New album is released!</h2>
-          <p>Click the button to listen on Spotiwhy app.</p>
+      <div className="bg-base-100 shadow-xl rounded-3xl min-h-[55vh] grid gap-2">
+        <div className="card lg:card-side">
+          <div className="card-body basis-3/4 grid gap-2 font-title text-center text-[clamp(2rem,8vw,4rem)] font-black leading-none xl:text-start">
+            <h4>Innovation</h4>
+            <h4 className="bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] bg-clip-text will-change-auto [transform:translate3d(0,0,0)] [-webkit-text-fill-color:transparent] motion-reduce:!tracking-normal max-[1279px]:!tracking-normal [@supports(color:oklch(0_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]">
+              Qualité assurée
+            </h4>
+            <h4>Relation</h4>
+          </div>
+          <figure>
+            <img src={pen} alt="" className="bg-neutral rounded-s-3xl " />
+          </figure>
+        </div>
+        <div className="flex w-full justify-center xl:justify-start m-2">
+          <a href="#histoire" className="btn btn-lg btn-wide group">
+            Notre histoire
+            <BsArrowRight />
+          </a>
         </div>
       </div>
 
