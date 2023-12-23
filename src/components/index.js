a139464942edb3e196f5e28d4f7fc9c84475d7e8
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-12-23 12:21:33 -0500

    Add components to index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 export { default as Header } from "./Header";
 export { default as Navbar } from "./Navbar";
 export { default as NavLinks } from "./NavLinks";
+export { default as Hero } from "./Hero";
+export { default as ErrorElement } from "./ErrorElement";
+// export { default as ParallaxScroll } from "./ParallaxScroll";
+// export { default as Navbar2 } from "./Navbar2";
