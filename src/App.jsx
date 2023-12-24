fb837a8f96fa041e9ea6a1864d65840c5e9ec31a
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-12-23 19:55:58 -0500

    separate routes handling from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,6 @@
-import React from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import {
-  Landing,
-  Cart,
-  About,
-  Checkout,
-  Error,
-  HomeLayout,
-  Login,
-  Orders,
-  SingleProduct,
-  Products,
-  Register,
-} from "./pages";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./route";
 
-import { ErrorElement } from "./components";
-
-// loaders
-import { loader as landingLoader } from "./pages/Landing";
-// actions
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomeLayout />,
-    errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        element: <Landing />,
-        errorElement: <ErrorElement />,
-        loader: landingLoader,
-      },
-      { path: "/products", element: <Products /> },
-      { path: "/products/:id", element: <SingleProduct /> },
-      { path: "/cart", element: <Cart /> },
-      { path: "/checkout", element: <Checkout /> },
-      { path: "/orders", element: <Orders /> },
-      { path: "/about", element: <About /> },
-    ],
-  },
-  { path: "/login", element: <Login />, errorElement: <Error /> },
-  { path: "/register", element: <Register />, errorElement: <Error /> },
-]);
 function App() {
   return <RouterProvider router={router} />;
 }
---
+++ src/route.jsx
@@ -0,0 +1,45 @@
+import { createBrowserRouter } from "react-router-dom";
+import {
+  Landing,
+  Cart,
+  About,
+  Checkout,
+  Error,
+  HomeLayout,
+  Login,
+  Orders,
+  SingleProduct,
+  Products,
+  Register,
+} from "./pages";
+
+//like 404 page
+import { ErrorElement } from "./components";
+
+// loaders --- data loader
+import { loader as landingLoader } from "./pages/Landing";
+
+// actions
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+        errorElement: <ErrorElement />,
+        loader: landingLoader,
+      },
+      { path: "/products", element: <Products /> },
+      { path: "/products/:id", element: <SingleProduct /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/checkout", element: <Checkout /> },
+      { path: "/orders", element: <Orders /> },
+      { path: "/about", element: <About /> },
+    ],
+  },
+  { path: "/login", element: <Login />, errorElement: <Error /> },
+  { path: "/register", element: <Register />, errorElement: <Error /> },
+]);
