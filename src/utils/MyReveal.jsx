2912415e54c26708ed18825be3b6812d5f28ff42
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-12-27 16:39:14 -0500

    Add basic animation

---
+++ src/utils/MyReveal.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from "react";
+import { motion, useInView, useAnimation } from "framer-motion";
+
+const Reveal = ({ children }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+  const mainControls = useAnimation();
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    }
+  }, [isInView]);
+  return (
+    <>
+      <motion.div
+        ref={ref}
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 0.5, delay: 0.25 }}
+      >
+        {children}
+      </motion.div>
+    </>
+  );
+};
+
+export default Reveal;
