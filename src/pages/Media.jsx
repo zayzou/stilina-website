15580cfa9f9280457f8f1fc61fdf7ae6a04eb109
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-12-29 16:34:02 -0500

    improve media page

--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -2,8 +2,12 @@ import MyReveal from "../utils/MyReveal";
 
 import { useFetchMedia } from "../data";
 import { formatDate } from "../utils";
+import { useRef, useState } from "react";
+
 function Media() {
   const { loading, media } = useFetchMedia();
+  const [hovered, setHovered] = useState(false);
+  const ref = useRef(null);
   return (
     <>
       <MyReveal>
@@ -18,27 +22,42 @@ function Media() {
       </MyReveal>
       <div className="flex flex-col gap-4 py-10"></div>
       <MyReveal>
-        <div className="grid grid-cols-2 gap-9 ">
+        <div
+          className="grid grid-cols-2  gap-4"
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+        >
           {media.map(({ id, title, description, tag, img, date }) => {
             return (
               <a
-                href="#"
+                href={`/media/${id}`}
+                className="card  bg-base-100 shadow-xl "
                 key={id}
-                className="card card-side bg-base-200 shadow-xl rounded-md transition-all duration-300 ease-in-out transform hover:scale-110 "
               >
-                <figure>
-                  <img className="h-full w-60" src={img} alt={title} />
+                <figure className="px-10 pt-10">
+                  <img
+                    ref={ref}
+                    src={img}
+                    alt={title}
+                    className="rounded-xl h-[250px] w-full"
+                    style={{
+                      width: hovered ? "90% !important" : "85% !important",
+                      rotate: hovered ? "2deg" : "0deg",
+                      transition: "0.5s all",
+                    }}
+                  />
                 </figure>
-                <div className="card-body p-4">
-                  <div className="flex justify-between items-center mb-5 ">
-                    <div className="badge badge-success badge-outline badge-md ">
-                      {tag}
-                    </div>
-                    <span className="text-sm">{formatDate(date)}</span>
+                <div className="card-body  ">
+                  <div className="badge badge-success  badge-md self-start ">
+                    {tag}
                   </div>
                   <h2 className="card-title">{title}</h2>
-                  <p className="font-light">{description}</p>
-                  <div className="card-actions justify-end"></div>
+                  <p className="text-base-content/70 font-title py-4 font-light">
+                    {description}
+                  </p>
+                  <div className="card-actions">
+                    <span className="text-sm">Le {formatDate(date)}</span>
+                  </div>
                 </div>
               </a>
             );
