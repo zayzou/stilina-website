229c8b061e0f7a964403a67c6e2154ebf521876c
Soffi Zahir <soffizahir2016@gmail.com>
2024-01-16 16:00:36 -0500

    set amount

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -2,6 +2,7 @@ import { Link, useLoaderData } from "react-router-dom";
 import { customAxiosInstance } from "../utils/constants";
 import { formatPrice } from "../utils/helpers";
 import { useState } from "react";
+import { FaPlus, FaMinus } from "react-icons/fa";
 export const loader = async ({ params }) => {
   const response = await customAxiosInstance.get(`/products/${params.id}`);
   return { product: response.data.data };
@@ -11,6 +12,7 @@ const SingleProduct = () => {
   const { image, title, price, description, category, colors, company } =
     product.attributes;
   const [productColor, setProductColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(0);
   const dinarAmount = formatPrice(price);
   return (
     <section>
@@ -55,6 +57,32 @@ const SingleProduct = () => {
                   ></button>
                 );
               })}
+            </div>
+            <div className="mt-2">
+              <h3 className="text-md font-medium tracking-wider capitalize">
+                Quantité
+              </h3>
+              <div className="join mt-2">
+                <button
+                  className="btn btn-ghost join-item"
+                  onClick={() => amount > 0 && setAmount(amount - 1)}
+                >
+                  <FaMinus />
+                </button>
+                <input
+                  type="text"
+                  className="join-item text-xl w-16 text-center "
+                  value={amount}
+                  readOnly
+                />
+
+                <button
+                  className="btn btn-ghost join-item"
+                  onClick={() => setAmount(amount + 1)}
+                >
+                  <FaPlus />
+                </button>
+              </div>
             </div>
           </div>
         </div>
