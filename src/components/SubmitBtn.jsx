04b3473507eb3429ea39ed564c0f3c8038067c36
Soffi Zahir <soffizahir2016@gmail.com>
2024-02-29 17:19:38 -0500

    Add loading spinner

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -1,8 +1,18 @@
+import {useNavigate} from "react-router-dom";
+
 const SubmitBtn = ({text, style}) => {
+
+    const navigation = useNavigate();
+    const isSubmitting = navigation.state === "submitting"
     return (
         <div className="card-actions justify-stretch">
-            <button type="submit" className={`btn flex-auto  ${style}`}>
-                {text}
+            <button disabled={isSubmitting} type="submit" className={`btn flex-auto  ${style}`}>
+                {
+                    isSubmitting ?
+                        <><span className="loading loading-spinner"></span>En cours...</>
+                        :
+                        text
+                }
             </button>
         </div>
     )
