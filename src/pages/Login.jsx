9eace01059d8903b56d7f3b9f9adb5f972086e2d
Soffi Zahir <soffizahir2016@gmail.com>
2024-02-29 16:30:57 -0500

    create login component

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import {FormInput, SubmitBtn} from "../components/index.js";
+
+const Login = () => {
+    return (
+        <div>
+            <FormInput/>
+            <SubmitBtn/>
+        </div>
+    )
+}
+
+export default Login;
--- src/utils/route.jsx
+++ src/utils/route.jsx
@@ -1,60 +1,62 @@
-import { createBrowserRouter } from "react-router-dom";
+import {createBrowserRouter} from "react-router-dom";
 import {
-  Landing,
-  Cart,
-  About,
-  Checkout,
-  Error,
-  HomeLayout,
-  Orders,
-  SingleProduct,
-  Products,
-  Brands,
-  Media,
-  Contact,
-  SingleMedia,
+    Landing,
+    Cart,
+    About,
+    Checkout,
+    Error,
+    HomeLayout,
+    Orders,
+    SingleProduct,
+    Products,
+    Brands,
+    Media,
+    Contact,
+    SingleMedia,
+    Login
 } from "../pages";
 
 //like 404 page
-import { ErrorElement } from "../components";
+import {ErrorElement} from "../components";
 
 // loaders --- data loader
-import { loader as landingLoader } from "../pages/Landing";
-import { loader as singleProductLoader } from "../pages/SingleProduct";
-import { loader as productLoader } from "../pages/Products";
+import {loader as landingLoader} from "../pages/Landing";
+import {loader as singleProductLoader} from "../pages/SingleProduct";
+import {loader as productLoader} from "../pages/Products";
 // actions
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomeLayout />,
-    errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        element: <Landing />,
-        errorElement: <ErrorElement />,
-        loader: landingLoader,
-      },
-      {
-        path: "/products",
-        element: <Products />,
-        loader: productLoader,
-        errorElement: <ErrorElement />,
-      },
-      {
-        path: "/products/:id",
-        element: <SingleProduct />,
-        loader: singleProductLoader,
-        errorElement: <ErrorElement />,
-      },
-      { path: "/cart", element: <Cart /> },
-      { path: "/checkout", element: <Checkout /> },
-      { path: "/orders", element: <Orders /> },
-      { path: "/about", element: <About /> },
-      { path: "/brands", element: <Brands /> },
-      { path: "/media", element: <Media /> },
-      { path: "/contact", element: <Contact /> },
-    ],
-  },
-  { path: "/media/:id", element: <SingleMedia />, errorElement: <Error /> },
+    {
+        path: "/",
+        element: <HomeLayout/>,
+        errorElement: <Error/>,
+        children: [
+            {
+                index: true,
+                element: <Landing/>,
+                errorElement: <ErrorElement/>,
+                loader: landingLoader,
+            },
+            {
+                path: "/products",
+                element: <Products/>,
+                loader: productLoader,
+                errorElement: <ErrorElement/>,
+            },
+            {
+                path: "/products/:id",
+                element: <SingleProduct/>,
+                loader: singleProductLoader,
+                errorElement: <ErrorElement/>,
+            },
+            {path: "/cart", element: <Cart/>},
+            {path: "/checkout", element: <Checkout/>},
+            {path: "/orders", element: <Orders/>},
+            {path: "/about", element: <About/>},
+            {path: "/brands", element: <Brands/>},
+            {path: "/media", element: <Media/>},
+            {path: "/login", element: <Login/>},
+            {path: "/contact", element: <Contact/>},
+        ],
+    },
+    {path: "/media/:id", element: <SingleMedia/>, errorElement: <Error/>},
 ]);
