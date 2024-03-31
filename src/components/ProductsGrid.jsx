446209449eeccf448a5e457691575ab807de15b1
Soffi Zahir <soffizahir2016@gmail.com>
2024-03-31 15:00:33 -0400

    render product image or loading container

--- src/components/ProductsGrid.jsx
+++ src/components/ProductsGrid.jsx
@@ -7,17 +7,21 @@ function ProductsGrid() {
     return (
         <div className=" grid gap-4 md:grid-cols-2 lg:grid-cols-3">
             {
-                products.map(({id, name, image, brand}) => {
+                products.map(({id, name, productImage: image, brand}) => {
+
                     return (
                         <Reveal key={id}>
                             <Link to={`/products/${id}`}
                                   className="card rounded-none w-full shadow-xl hover:shadow-2xl transition duration-300">
                                 <figure className="px-4 pt-4">
-                                    <img
-                                        src={image ? image : placeholder}
-                                        alt={name}
-                                        className="h-64 md:h-48 w-full object-cover"
-                                    />
+                                    {image ?
+                                        <img
+                                            src={image}
+                                            alt={name}
+                                            className="h-64 md:h-48 w-full object-cover"
+                                        /> :
+                                        <div className="skeleton h-64 md:h-48 w-full"></div>
+                                    }
                                 </figure>
                                 <div className="card-body">
                                     <h2 className="card-title capitalize tracking-wider text-sm">
